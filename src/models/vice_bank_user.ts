1cc272db4964bcba115ef8bea8055907ac3e113c
Mathew Thompson <mathewthompson@Mathews-MacBook-Air.local>
2025-05-04 07:38:06 -0500

    Removing the vbUserId value. We're not going to worry about multiple vice banks per user.

--- src/models/vice_bank_user.ts
+++ src/models/vice_bank_user.ts
@@ -1,20 +1,13 @@
 import { InvalidInputError } from '../utils/errors';
-import {
-  isNumber,
-  isString,
-  typeGuardGenerator,
-  typeGuardTestGenerator,
-} from 'tcheck';
+import { isString, typeGuardGenerator, typeGuardTestGenerator } from 'tcheck';
 
 export interface ViceBankUserJSON {
   id: string;
-  userId: string;
   name: string;
 }
 
 const isViceBankUserJSONCommon = {
   id: isString,
-  userId: isString,
   name: isString,
 };
 const isViceBankUserJSON = typeGuardGenerator<ViceBankUserJSON>(
@@ -24,20 +17,15 @@ const isViceBankUserJSONTest = typeGuardTestGenerator(isViceBankUserJSONCommon);
 
 export class ViceBankUser {
   protected _id: string;
-  protected _userId: string;
   protected _name: string;
 
   constructor(payload: ViceBankUserJSON) {
     this._id = payload.id;
-    this._userId = payload.userId;
     this._name = payload.name;
   }
 
   get id(): string {
     return this._id;
-  }
-  get userId(): string {
-    return this._userId;
   }
   get name(): string {
     return this._name;
@@ -46,7 +34,6 @@ export class ViceBankUser {
   toJSON(): ViceBankUserJSON {
     return {
       id: this.id,
-      userId: this.userId,
       name: this.name,
     };
   }
